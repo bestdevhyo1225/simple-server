dcea32e314051de05a167a5cd2e8a423f6fc1a0a
janghyoseog <bestdevhyo1225@gmail.com>
2020-08-31 05:09:08 +0900

    chore: JPA Audit Config 파일에 default 값 추가

--- src/main/java/com/runjook/adapter/configuration/JpaAuditConfig.java
+++ src/main/java/com/runjook/adapter/configuration/JpaAuditConfig.java
@@ -1,9 +1,19 @@
 package com.runjook.adapter.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "defaultAuditorAware")
 public class JpaAuditConfig {
+
+    @Bean
+    public AuditorAware<String> defaultAuditorAware(){
+        return () -> Optional.of("default");
+    }
+
 }
